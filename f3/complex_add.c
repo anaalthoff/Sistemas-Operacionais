673082d5631df69be5f0b7b626de582bf594cd65
anaalthoff <anapaula.althoff@gmail.com>
2025-11-10 09:47:56 +0000

    feat: adds complex_add function to add complex numbers

--- f3/complex_add.c
+++ f3/complex_add.c
@@ -6,7 +6,7 @@ complex *complex_add(complex *z, complex *w)
     return complex_new(z->x + w->x, z->y + w->y);
 }
 
-// ->x → parte real de z
+// z->x → parte real de z
 // z->y → parte imaginária de z
 // w->x → parte real de w
 // w->y → parte imaginária de w
