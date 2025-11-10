38a751efbb68cd159afd67c64be1a0ea62beb96b
anaalthoff <anapaula.althoff@gmail.com>
2025-11-10 09:55:34 +0000

    refactor: add comments to complex multiplication function

--- f3/complex_mul.c
+++ f3/complex_mul.c
@@ -5,3 +5,7 @@ complex *complex_mul(complex *z, complex *w)
     return complex_new(z->x * w->x - z->y * w->y,
                        z->x * w->y + z->y * w->x);
 }
+
+// i² = -1
+// por isso o produto das partes imaginárias é subtraído da parte real
+// e o produto cruzado das partes real e imaginária é somado na parte imaginária
